aaad1945b377f971cc04c3b41f2011c1e0f3b965
Aldwin Lewis <aldwin.lewis@gmail.com>
2017-02-08 22:10:40 -0500

    initial commit

---
+++ WeatherApp/WeatherApp/Core.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "YOUR API KEY HERE";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q="
+                + zipCode + "&units=metric&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false); // GET-datakald - se DataService.cs - JSON-objekt retur
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + "° C";
+                weather.Wind = (string)results["wind"]["speed"] + " m/sek";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather; // Parset og sat ind i weather-objekt
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+            this.Title = "Sample Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+            //Set the default binding to a default object for now
+            this.BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e) // Statisk klasse, der overskriver eksisterende vejrdata og indsætter nye
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text); 
+                if (weather != null)
+                {
+                    this.BindingContext = weather;
+                    getWeatherBtn.Text = "Search Again";
+                }
+            }
+        }
+    }
+}
